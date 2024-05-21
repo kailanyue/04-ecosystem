a48577d4b620f9de8da6fffb65669f02bdb547cb
kailanyue <yuekailan@163.com>
2024-05-21 15:06:38 +0800

    feature: introduce tracing & tracing-subscriber, opentelemetry-otlp for tracing

--- examples/error.rs
+++ examples/error.rs
@@ -1,5 +1,4 @@
 use anyhow::Context;
-use ecosystem::MyError;
 use std::{fs, mem::size_of, num::ParseIntError};
 
 fn main() -> Result<(), anyhow::Error> {
@@ -30,4 +29,33 @@ fn main() -> Result<(), anyhow::Error> {
 
 fn fail_with_error() -> Result<(), MyError> {
     Err(MyError::Custom("This is a custom error".to_string()))
+}
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Parse error: {0}")]
+    Parse(#[from] std::num::ParseIntError),
+
+    #[error("Serialize json error: {0}")]
+    Serialize(#[from] serde_json::Error),
+
+    #[error("Error: {0:?}")]
+    BigError(Box<BigError>),
+
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8; 64],
+    d: u64,
 }
